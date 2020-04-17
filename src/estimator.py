bd85c30f373271317cba1ea715bf1ba553607047
delat22 <babalolalateef18@gmai.com>
2020-04-17 01:19:34 +0100

    Set up workspace

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,27 @@
+
+
+data = {
+    "region": {
+        "name": "Africa",
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 5,
+        "avgDailyIncomePopulation": 0.71
+    },
+    "periodType": "days",
+    "timeToElapse": 58,
+    "reportedCases": 674,
+    "population": 66622705,
+    "totalHospitalBeds": 1380614
+}
+
+
 def estimator(data):
-  return data
+  
+  impact = {
+
+  }
+
+  severeImpact = {
+
+  }
+  return {"data": data, "impact": impact, "severeImpact": severeImpact} 
