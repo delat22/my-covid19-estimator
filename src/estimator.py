401e99bec0baab563195f3d58bab4b129038b826
delat22 <babalolalateef18@gmai.com>
2020-04-21 12:00:29 +0100

    fix bug

--- src/estimator.py
+++ src/estimator.py
@@ -1,31 +1,4 @@
-import math
 
-data = {
-    "region": {
-        "name": "Africa",
-        "avgAge": 19.7,
-        "avgDailyIncomeInUSD": 5,
-        "avgDailyIncomePopulation": 0.71
-    },
-    "periodType": "days",
-    "timeToElapse": 58,
-    "reportedCases": 674,
-    "population": 66622705,
-    "totalHospitalBeds": 1380614
-}
-
-
-def timeEstimate(currentlyInfected, time):
-  factor = math.trunc(time/3)
-  return currentlyInfected*(2**factor)
-
-def getTime(duration, figure):
-  if duration == 'months':
-    return figure*30
-  elif duration == 'weeks':
-    return figure*7
-  else:
-    return figure
 def estimator(data):
   days = getTime(data['periodType'], data['timeToElapse'])
 
@@ -48,3 +21,14 @@ def estimator(data):
   output = {"data": data, "impact": impact, "severeImpact": severeImpact}
   return output
 
+def timeEstimate(currentlyInfected, time):
+  factor = time//3
+  return currentlyInfected*(2**factor)
+
+def getTime(duration, figure):
+  if duration == 'months':
+    return figure*30
+  elif duration == 'weeks':
+    return figure*7
+  else:
+    return figure
